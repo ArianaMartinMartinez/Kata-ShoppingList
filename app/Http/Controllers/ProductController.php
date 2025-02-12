75945a83994cace631ef4d23cca0cff202b8d6af
Ariana <104553851+ArianaMartinMartinez@users.noreply.github.com>
2025-02-12 09:27:26 +0100

    feat: ProductController structure

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(string $id)
+    {
+        //
+    }
+
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    public function destroyOneProduct(string $id)
+    {
+        //
+    }
+
+    public function destroyAllProducts() {
+        //
+    }
+}
