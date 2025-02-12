3eb463a01e95358bf396f76f13ffb6c94a2839e2
Ariana <104553851+ArianaMartinMartinez@users.noreply.github.com>
2025-02-12 09:37:32 +0100

    feat: add index and show routes for Products

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/products', [ProductController::class, 'index'])->name('apiHomeProducts');
+Route::get('/products/{id}', [ProductController::class, 'show'])->name('apiShowProduct');
